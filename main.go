783d8da7db71986090c4c981b6497a1c0a391848
Taylor Monacelli <taylormonacelli@gmail.com>
2023-08-01 06:00:10 -0700

    close but not quite

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -13,8 +14,9 @@ import (
 var logFilePath = flag.String("path", "/var/log/cloud-init-output.log", "Path to the log file")
 
 type FileState struct {
-	LastPos int64
-	Mux     sync.Mutex
+	LastPos  int64
+	Mux      sync.Mutex
+	StartTime time.Time
 }
 
 func init() {
@@ -28,7 +30,7 @@ func (fs *FileState) MonitorFile(logFilePath string) {
 	file, err := os.Open(logFilePath)
 	if err != nil {
 		if !os.IsNotExist(err) {
-			log.Printf("%s - File error: %s", logFilePath, err.Error())
+			log.Printf("File error: %s - %s", logFilePath, err.Error())
 		}
 		return
 	}
@@ -36,7 +38,7 @@ func (fs *FileState) MonitorFile(logFilePath string) {
 
 	fileInfo, err := file.Stat()
 	if err != nil {
-		log.Printf("%s - Stat error: %s", logFilePath, err.Error())
+		log.Printf("Stat error: %s - %s", logFilePath, err.Error())
 		return
 	}
 
@@ -46,7 +48,7 @@ func (fs *FileState) MonitorFile(logFilePath string) {
 
 	_, err = file.Seek(fs.LastPos, 0)
 	if err != nil {
-		log.Printf("%s - Seek error: %s", logFilePath, err.Error())
+		log.Printf("Seek error: %s - %s", logFilePath, err.Error())
 		return
 	}
 
@@ -55,14 +57,16 @@ func (fs *FileState) MonitorFile(logFilePath string) {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err != io.EOF {
-				log.Printf("%s - Read error: %s", logFilePath, err.Error())
+				log.Printf("Read error: %s - %s", logFilePath, err.Error())
 			}
 			break
 		}
-		log.Printf("%s - %s", logFilePath, line)
+		relativeTime := time.Since(fs.StartTime).Round(time.Second)
+		line = strings.TrimSpace(line)
+		log.Printf("%s - %s - Relative Time: %s", logFilePath, relativeTime ,line )
 		fs.LastPos, err = file.Seek(0, io.SeekCurrent)
 		if err != nil {
-			log.Printf("%s - Seek error: %s", logFilePath, err.Error())
+			log.Printf("Seek error: %s - %s", logFilePath, err.Error())
 			break
 		}
 	}
@@ -71,7 +75,9 @@ func (fs *FileState) MonitorFile(logFilePath string) {
 func main() {
 	flag.Parse()
 	ticker := time.NewTicker(500 * time.Millisecond)
-	fileState := &FileState{}
+	fileState := &FileState{
+		StartTime: time.Now(),
+	}
 	go func() {
 		for range ticker.C {
 			fileState.MonitorFile(*logFilePath)
